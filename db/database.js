020415b2ca0dec275f4e2404a67095a7f6e270cc
= <=>
2025-04-23 11:34:43 +0700

    edit env db

--- db/database.js
+++ db/database.js
@@ -1,5 +1,5 @@
 const Database = require('better-sqlite3');
-const db = new Database('inventory.db');
+const db = new Database(process.env.DB_PATH || 'inventory.db');
 
 db.exec(`
 CREATE TABLE IF NOT EXISTS categories (
